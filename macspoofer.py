73c24f86c2ac8c8a5cd7ce1accf5777a7b6db7fd
chill tamizha <43850019+chilltamizha@users.noreply.github.com>
2021-01-13 19:05:25 +0530

    Update macspoofer.py

--- macspoofer.py
+++ macspoofer.py
@@ -1,4 +1,4 @@
 import subprocess
-subprocess.call("if config eth0 down", shell=True, check=True)
-subprocess.call("if config eth0 hw 00:65:55:44:33:22", shell=True, check=True)
-subprocess.call("if config eth0 up", shell=True, check=True)
+subprocess.call("if config eth0 down", shell=True)
+subprocess.call("if config eth0 hw ether 00:65:55:44:33:22", shell=True)
+subprocess.call("if config eth0 up", shell=True)
